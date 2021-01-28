ac54966e4853376cc7711dcd6f67d3e70c64e283
Miguel Remedios <1919716@brunel.ac.uk>
2021-01-28 14:53:45 +0000

    Creating Recipe Controller and Service class

--- TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipe/RecipeService.java
+++ TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipe/RecipeService.java
@@ -1,5 +1,22 @@
 package group43.thesilentingredient.recipe;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RecipeService {
 
+	private final RecipeRepository recipeRepository;
+	
+	@Autowired
+	public RecipeService(RecipeRepository recipeRepository) {
+		this.recipeRepository = recipeRepository;
+	}
+	
+	
+	public List<Recipe> getRecipes() {
+		return recipeRepository.findAll();
+	}
 }
