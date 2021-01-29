4411c49f85467ff80f25ae61ab3b2a23682fc0c5
Miguel Remedios <1919716@brunel.ac.uk>
2021-01-29 00:27:07 +0000

    Update Recipe Table classes SB

--- TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipe/RecipeController.java
+++ TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipe/RecipeController.java
@@ -1,11 +1,17 @@
 package group43.thesilentingredient.recipe;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(path= "api/v1/recipe")
@@ -22,6 +28,16 @@ public class RecipeController {
 	@GetMapping
 	public List<Recipe> getRecipes() {
 		return recipeService.getRecipes();		
+	}
+	
+	@PostMapping
+	public void registerRecipe(@RequestBody Recipe recipe) {
+		recipeService.addNewRecipe(recipe);
+	}
+
+	@DeleteMapping(path = "{recipeId}")
+	public void deleteRecipe(@PathVariable("recipeId") Long recipeId) {
+		recipeService.deleteRecipe(recipeId);
 	}
 	
 }
--- TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipe/RecipeService.java
+++ TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipe/RecipeService.java
@@ -1,9 +1,12 @@
 package group43.thesilentingredient.recipe;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class RecipeService {
@@ -19,4 +22,29 @@ public class RecipeService {
 	public List<Recipe> getRecipes() {
 		return recipeRepository.findAll();
 	}
+	
+	
+	public void addNewRecipe(Recipe recipe) {
+		Optional<Recipe> recipeOptional = recipeRepository.findRecipeByName(recipe.getName());
+
+		if (recipeOptional.isPresent()) {
+			throw new IllegalStateException("Recipe taken!");
+		}
+
+		recipeRepository.save(recipe);
+	}
+
+
+	public void deleteRecipe(Long recipeId) {
+
+		boolean exists = recipeRepository.existsById(recipeId);
+
+		if(!exists) {
+			throw new IllegalStateException("Recipe with ID " + recipeId + " does not exist!");
+		}
+
+		recipeRepository.deleteById(recipeId);
+	}
+
+
 }
