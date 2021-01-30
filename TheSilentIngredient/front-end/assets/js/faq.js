a6d8a811137c2737bd225c0c3aa660f62e5eec38
Miguel Remedios <1919716@brunel.ac.uk>
2021-01-30 00:17:16 +0000

    Adding FAQ section / Bug Fixing

--- TheSilentIngredient/front-end/assets/js/faq.js
+++ TheSilentIngredient/front-end/assets/js/faq.js
@@ -3,11 +3,11 @@ const accordionItemHeaders = document.querySelectorAll(".accordion-item-header")
 accordionItemHeaders.forEach(accordionItemHeader => {
   accordionItemHeader.addEventListener("click", event => {
  
-    //const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-    //if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-    //   currentlyActiveAccordionItemHeader.classList.toggle("active");
-    //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-    // }
+    const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+    if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+       currentlyActiveAccordionItemHeader.classList.toggle("active");
+       currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+     }
 
     accordionItemHeader.classList.toggle("active");
     const accordionItemBody = accordionItemHeader.nextElementSibling;
