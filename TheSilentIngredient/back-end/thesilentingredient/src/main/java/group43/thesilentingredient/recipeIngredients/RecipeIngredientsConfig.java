ee0bb669e3d17b331e09bd0959251d19b76406d2
MatthewGarner572 <MatthewGarner572@users.noreply.github.com>
2021-02-01 18:46:41 +0000

    Create Tables RecipeIngredients
    Co-Authored-By: ViktorAtanasov4 <73655603+ViktorAtanasov4@users.noreply.github.com>

--- TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipeIngredients/RecipeIngredientsConfig.java
+++ TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipeIngredients/RecipeIngredientsConfig.java
@@ -1,5 +1,18 @@
 package group43.thesilentingredient.recipeIngredients;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RecipeIngredientsConfig {
 
+	@Bean
+	CommandLineRunner RecipeIngredientsCR(RecipeIngredientsRepository repository) {
+		return args -> {
+			RecipeIngredients chickenmasala1 = new RecipeIngredients(1,1,8.0,"quantity");
+			RecipeIngredients chickenmasala2 = new RecipeIngredients(2,1,400,"g");
+			RecipeIngredients chickenmasala3 = new RecipeIngredients(3,1,150,"ml");
+		};
+	}
 }
--- TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipeIngredients/RecipeIngredientsRepository.java
+++ TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipeIngredients/RecipeIngredientsRepository.java
@@ -1,5 +1,23 @@
 package group43.thesilentingredient.recipeIngredients;
 
-public interface RecipeIngredientsRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import group43.thesilentingredient.recipe.Recipe;
+
+
+@Repository
+public interface RecipeIngredientsRepository extends JpaRepository<RecipeIngredients, Long>{
+	
+//	//SELECT * FROM recipe WHERE name = ?
+//	@Query("SELECT r FROM Recipe r WHERE r.name = ?1")
+//	Optional<Recipe> findRecipeByName(String name);
+//	
+//	//SELECT * FROM recipe WHERE description = ?
+//	@Query("SELECT d FROM Recipe d WHERE d.description = ?1")
+//	Optional<Recipe> findRecipeByDescription(String description);
+	
 }
