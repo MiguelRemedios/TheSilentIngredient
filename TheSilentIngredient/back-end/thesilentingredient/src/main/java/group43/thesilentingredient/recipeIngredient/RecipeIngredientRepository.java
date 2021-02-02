1bfb39e8a65a8c61d4cf3165b1af51c639bea90d
MatthewGarner572 <MatthewGarner572@users.noreply.github.com>
2021-02-02 21:39:20 +0000

    Fixed Recipe ID mapping
    Co-Authored-By: ViktorAtanasov4 <73655603+ViktorAtanasov4@users.noreply.github.com>

--- TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipeIngredient/RecipeIngredientRepository.java
+++ TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipeIngredient/RecipeIngredientRepository.java
@@ -1,22 +1,25 @@
 package group43.thesilentingredient.recipeIngredient;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import group43.thesilentingredient.recipe.Recipe;
+
 @Repository
 public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long>{
 
-	//SELECT * FROM recipe WHERE name = ?
-	//SELECT * FROM recipe_ingredient WHERE recipe_id = 1;
-	
-//	@Query("SELECT x FROM recipe_ingredient x WHERE x.recipe_id = ?1")
-//	Optional<RecipeIngredient> findRecipeByIngredients(int recipe_id);
+	//SELECT * FROM recipe_ingredient WHERE recipe_id = ?;
+	@Query("SELECT x FROM RecipeIngredient x WHERE x.recipe_id = ?1")
+	List<RecipeIngredient> findIngredientsByRecipe_id(int recipe_id);
 
-//	//SELECT * FROM recipe WHERE description = ?
-//	@Query("SELECT m FROM Recipe_Ingredient m WHERE m.measurement = ?1")
-//	Optional<RecipeIngredient> findRecipeByDescription(String measurement);
+	//SELECT count(recipe_id) FROM recipe_ingredient;
+	@Query("SELECT count(*) FROM RecipeIngredient WHERE recipe_id = ?1")
+	List<RecipeIngredient> countIngredients(int recipe_id);
+
+		
 
 }
