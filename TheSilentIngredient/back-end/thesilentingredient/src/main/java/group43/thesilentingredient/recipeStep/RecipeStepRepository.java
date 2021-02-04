5d83fc0e216bb2e0aa3a7e1d08f74df30973e496
Smit Maniar <siimss.man@gmail.com>
2021-02-04 00:01:10 +0000

    Created Config and User Repo for Steps

---
+++ TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipeStep/RecipeStepRepository.java
@@ -0,0 +1,22 @@
+package group43.thesilentingredient.recipeStep;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeStepRepository extends JpaRepository<RecipeStep, Long>{
+	//SELECT * FROM recipe_step WHERE recipe_id = ?;
+	@Query("SELECT x FROM RecipeStep x WHERE x.recipe_id = ?1")
+	List<RecipeStep> findStepsByRecipe_id(String step);
+
+	//SELECT count(recipe_id) FROM recipe_step;
+	@Query("SELECT count(*) FROM RecipeStep WHERE recipe_id = ?1")
+	List<RecipeStep> countSteps(Integer step_id);
+
+	//SELECT count(recipe_id) FROM recipe_step;
+	
+}
