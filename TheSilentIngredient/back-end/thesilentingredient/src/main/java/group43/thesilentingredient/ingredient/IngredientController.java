1a90baf1de18e4a8b7b053ba6733ac738ed4abd9
MaraCorreia <maralscorreia@hotmail.com>
2021-02-05 03:00:24 +0000

    Ingredient backend update

--- TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/ingredient/IngredientController.java
+++ TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/ingredient/IngredientController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-/*@RestController
+@RestController
 @RequestMapping(path= "api/v1/recipe")
 @CrossOrigin(origins = "*")
 
