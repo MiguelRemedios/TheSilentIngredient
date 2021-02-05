983fa3b1affbf521f52631a6282cd9e9d2843e3f
MaraCorreia <maralscorreia@hotmail.com>
2021-02-05 13:31:08 +0000

    IngredientBackendUpdate
    Co-Authored-By: meli193 <77280038+meli193@users.noreply.github.com>

--- TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/ingredient/IngredientController.java
+++ TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/ingredient/IngredientController.java
@@ -1,4 +1,4 @@
-package com.example.demo.ingredient;
+package group43.thesilentingredient.ingredient;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
