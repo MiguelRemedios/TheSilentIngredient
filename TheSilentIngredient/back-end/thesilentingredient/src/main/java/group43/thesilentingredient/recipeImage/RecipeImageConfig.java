d62deddcfad2df4aecb6a88bd2fff530e0f14adf
Miguel Remedios <1919716@brunel.ac.uk>
2021-02-12 22:00:09 +0000

    Update RecipeImageConfig.java

--- TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipeImage/RecipeImageConfig.java
+++ TheSilentIngredient/back-end/thesilentingredient/src/main/java/group43/thesilentingredient/recipeImage/RecipeImageConfig.java
@@ -29,23 +29,23 @@ public class RecipeImageConfig {
 			RecipeImage NDWhiteRice4 = new RecipeImage(1,"http://127.0.0.1:5500/images/recipe/nodiet/NDWhiteRice4.jpg");
 			RecipeImage NDWhiteRice5 = new RecipeImage(1,"http://127.0.0.1:5500/images/recipe/nodiet/NDWhiteRice5.jpg");
 			//RECIPE 2
-			RecipeImage NDSpaghetti1 = new RecipeImage(2,"/images/recipe/nodiet/NDSpaghetti1.jpg");
-			RecipeImage NDSpaghetti2 = new RecipeImage(2,"/images/recipe/nodiet/NDSpaghetti2.jpg");
-			RecipeImage NDSpaghetti3 = new RecipeImage(2,"/images/recipe/nodiet/NDSpaghetti3.jpg");
-			RecipeImage NDSpaghetti4 = new RecipeImage(2,"/images/recipe/nodiet/NDSpaghetti4.jpg");
-			RecipeImage NDSpaghetti5 = new RecipeImage(2,"/images/recipe/nodiet/NDSpaghetti5.jpg");
+			RecipeImage NDSpaghetti1 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDSpaghetti1.jpg");
+			RecipeImage NDSpaghetti2 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDSpaghetti2.jpg");
+			RecipeImage NDSpaghetti3 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDSpaghetti3.jpg");
+			RecipeImage NDSpaghetti4 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDSpaghetti4.jpg");
+			RecipeImage NDSpaghetti5 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDSpaghetti5.jpg");
 			//RECIPE 3
-			RecipeImage NDBagel1 = new RecipeImage(2,"/images/recipe/nodiet/NDBagel1.jpg");
-			RecipeImage NDBagel2 = new RecipeImage(2,"/images/recipe/nodiet/NDBagel2.jpg");
-			RecipeImage NDBagel3 = new RecipeImage(2,"/images/recipe/nodiet/NDBagel3.jpg");
-			RecipeImage NDBagel4 = new RecipeImage(2,"/images/recipe/nodiet/NDBagel4.jpg");
-			RecipeImage NDBagel5 = new RecipeImage(2,"/images/recipe/nodiet/NDBagel5.jpg");
+			RecipeImage NDBagel1 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDBagel1.jpg");
+			RecipeImage NDBagel2 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDBagel2.jpg");
+			RecipeImage NDBagel3 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDBagel3.jpg");
+			RecipeImage NDBagel4 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDBagel4.jpg");
+			RecipeImage NDBagel5 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDBagel5.jpg");
 			//RECIPE 4
-			RecipeImage NDBurger1 = new RecipeImage(2,"/images/recipe/nodiet/NDBurger1.jpg");
-			RecipeImage NDBurger2 = new RecipeImage(2,"/images/recipe/nodiet/NDBurger2.jpg");
-			RecipeImage NDBurger3 = new RecipeImage(2,"/images/recipe/nodiet/NDBurger3.jpg");
-			RecipeImage NDBurger4 = new RecipeImage(2,"/images/recipe/nodiet/NDBurger4.jpg");
-			RecipeImage NDBurger5 = new RecipeImage(2,"/images/recipe/nodiet/NDBurger5.jpg");
+			RecipeImage NDBurger1 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDBurger1.jpg");
+			RecipeImage NDBurger2 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDBurger2.jpg");
+			RecipeImage NDBurger3 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDBurger3.jpg");
+			RecipeImage NDBurger4 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDBurger4.jpg");
+			RecipeImage NDBurger5 = new RecipeImage(2,"http://127.0.0.1:5500/images/recipe/nodiet/NDBurger5.jpg");
 			
 			repository.saveAll(List.of(NDWhiteRice1,NDWhiteRice2,NDWhiteRice3,NDWhiteRice4,NDWhiteRice5,
 									   NDSpaghetti1,NDSpaghetti2,NDSpaghetti3,NDSpaghetti4,NDSpaghetti5,
