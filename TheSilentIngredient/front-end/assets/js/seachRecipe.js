6706e2ade502c6b1e8f58f6c249d2ac659f06cfd
Abbasali Nasser <nasserabbasali@gmail.com>
2021-03-11 18:17:39 +0300

    Adding scroll to top button for recipeSearch page

--- TheSilentIngredient/front-end/assets/js/seachRecipe.js
+++ TheSilentIngredient/front-end/assets/js/seachRecipe.js
@@ -252,3 +252,23 @@ function removeEmpty(){
 }
 
 recipes();
+
+// Scroll to top button
+mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0;
+}
